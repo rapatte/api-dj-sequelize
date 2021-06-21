1cd64c1820fbec0b44204cc7a6bdd97b396f2e3b
rapatte <antoine.gayoso@gmai.com>
2021-06-21 12:40:08 +0200

    feat: sequalize initialization

---
+++ models/clubs.js
@@ -0,0 +1,25 @@
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class Clubs extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Clubs.init(
+    {
+      id: DataTypes.UUID,
+      name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Clubs",
+    }
+  );
+  return Clubs;
+};
