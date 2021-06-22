7b8460da54406b4cc8e627532b86e8111fc91bc3
antoine <antoine.gayoso@gmail.com>
2021-06-22 15:38:07 +0200

    fix: environmental variables updated

--- src/database/config.js
+++ src/database/config.js
@@ -4,22 +4,22 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: "database_development",
-    host: "127.0.0.1",
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql",
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: "database_test",
-    host: "127.0.0.1",
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql",
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: "database_production",
-    host: "127.0.0.1",
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql",
   },
 };
