f0ba0940c39e9ec424bfb5f7b036dcda83174632
antoine <antoine.gayoso@gmail.com>
2021-06-23 09:59:50 +0200

    fix: coherent camelcase

--- src/database/seeds/20200901155256-musical_genres.js
+++ src/database/seeds/20200901155256-musical_genres.js
@@ -1,11 +1,11 @@
 const now = new Date();
 
 module.exports = [
-  { id: 1, name: "Open Format", createdAt: now, updated_at: now },
-  { id: 2, name: "House", createdAt: now, updated_at: now },
-  { id: 3, name: "Deep House", createdAt: now, updated_at: now },
-  { id: 4, name: "Electro", createdAt: now, updated_at: now },
-  { id: 5, name: "Funk", createdAt: now, updated_at: now },
-  { id: 6, name: "Hip Hop", createdAt: now, updated_at: now },
-  { id: 7, name: "Rap", createdAt: now, updated_at: now },
+  { id: 1, name: "Open Format", createdAt: now, updatedAt: now },
+  { id: 2, name: "House", createdAt: now, updatedAt: now },
+  { id: 3, name: "Deep House", createdAt: now, updatedAt: now },
+  { id: 4, name: "Electro", createdAt: now, updatedAt: now },
+  { id: 5, name: "Funk", createdAt: now, updatedAt: now },
+  { id: 6, name: "Hip Hop", createdAt: now, updatedAt: now },
+  { id: 7, name: "Rap", createdAt: now, updatedAt: now },
 ];
