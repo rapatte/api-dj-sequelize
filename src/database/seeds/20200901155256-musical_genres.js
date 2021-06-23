bfb31a10085da50989dd2fffc948712037b21b4a
antoine <antoine.gayoso@gmail.com>
2021-06-23 10:05:34 +0200

    fix: define id with uuid

--- src/database/seeds/20200901155256-musical_genres.js
+++ src/database/seeds/20200901155256-musical_genres.js
@@ -1,11 +1,12 @@
 const now = new Date();
+const uuid = require("uuid");
 
 module.exports = [
-  { id: 1, name: "Open Format", createdAt: now, updatedAt: now },
-  { id: 2, name: "House", createdAt: now, updatedAt: now },
-  { id: 3, name: "Deep House", createdAt: now, updatedAt: now },
-  { id: 4, name: "Electro", createdAt: now, updatedAt: now },
-  { id: 5, name: "Funk", createdAt: now, updatedAt: now },
-  { id: 6, name: "Hip Hop", createdAt: now, updatedAt: now },
-  { id: 7, name: "Rap", createdAt: now, updatedAt: now },
+  { id: uuid(), name: "Open Format", createdAt: now, updatedAt: now },
+  { id: uuid(), name: "House", createdAt: now, updatedAt: now },
+  { id: uuid(), name: "Deep House", createdAt: now, updatedAt: now },
+  { id: uuid(), name: "Electro", createdAt: now, updatedAt: now },
+  { id: uuid(), name: "Funk", createdAt: now, updatedAt: now },
+  { id: uuid(), name: "Hip Hop", createdAt: now, updatedAt: now },
+  { id: uuid(), name: "Rap", createdAt: now, updatedAt: now },
 ];
