31f3981f895ed652385a2b22a2f01fe314e9e64d
eliezer-web <evj.97160@gmail.com>
2021-06-24 10:50:38 +0200

    wip

--- src/database/migrations/20210623085828-create-dj-musicalgenre.js
+++ src/database/migrations/20210623085828-create-dj-musicalgenre.js
@@ -11,7 +11,7 @@ module.exports = {
         type: Sequelize.UUID,
         allowNull: false,
         references: {
-          model: "Djs",
+          model: "Dj",
           key: "id",
         },
       },
